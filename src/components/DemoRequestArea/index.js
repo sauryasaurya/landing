96f419324b5736aa74025608096156e05d6dc993
Surya Prasad Bhandari <saurya.bhandari78792@gmail.com>
2023-09-29 22:32:47 +0545

    feat: add demo request page

---
+++ src/components/DemoRequestArea/index.js
@@ -0,0 +1,105 @@
+import Box from "@/common/components/Box";
+import Button from "@/common/components/Button";
+import Heading from "@/common/components/Heading";
+import Input from "@/common/components/Input";
+import NextImage from "@/common/components/NextImage";
+import Container from "@/common/components/UI/Container";
+import { technologyCommunity } from "@/common/data/AppCreative2";
+import Fade from "react-reveal/Fade";
+import Section, {
+	Content,
+	Figure,
+	FigureWrapper,
+	Grid,
+} from "./technology.style";
+
+const DemoRequestArea = () => {
+	return (
+		<Section>
+			<Container width="1400px">
+				<Grid>
+					<FigureWrapper>
+						<Box className="Right">
+							<Heading content="Request a Demo" />
+							<div
+								className="messageInput"
+								style={{ marginTop: "1rem", marginBottom: "1rem" }}
+							>
+								<Input
+									inputType="text"
+									placeholder="Full Name"
+									iconPosition="right"
+									isMaterial={false}
+									aria-label="name"
+								/>
+							</div>
+
+							<div
+								className="messageInput"
+								style={{ marginTop: "1rem", marginBottom: "1rem" }}
+							>
+								<Input
+									inputType="email"
+									placeholder="Business Email"
+									iconPosition="right"
+									isMaterial={false}
+									className="email_input"
+									aria-label="email"
+								/>
+							</div>
+
+							<div
+								className="messageInput"
+								style={{ marginTop: "1rem", marginBottom: "1rem" }}
+							>
+								<Input
+									inputType="text"
+									placeholder="Phone Number"
+									iconPosition="right"
+									isMaterial={false}
+									aria-label="contact"
+								/>
+							</div>
+							<div
+								className="messageInput"
+								style={{ marginTop: "1rem", marginBottom: "1rem" }}
+							>
+								<Input
+									inputType="text"
+									placeholder="Company Name"
+									iconPosition="right"
+									isMaterial={false}
+									aria-label="contact"
+								/>
+							</div>
+							<div
+								className="messageInput"
+								style={{ marginTop: "1rem", marginBottom: "1rem" }}
+							>
+								<Input
+									inputType="text"
+									placeholder="Job Title"
+									iconPosition="right"
+									isMaterial={false}
+									aria-label="contact"
+								/>
+							</div>
+
+							<Button title="Request Demo" />
+						</Box>
+					</FigureWrapper>
+
+					<Content>
+						<Fade up>
+							<Figure>
+								<NextImage src={technologyCommunity?.thumb} alt="analytics" />
+							</Figure>
+						</Fade>
+					</Content>
+				</Grid>
+			</Container>
+		</Section>
+	);
+};
+
+export default DemoRequestArea;
---
+++ src/pages/demo-request.jsx
@@ -0,0 +1,14 @@
+import ContactSection from "@/components/Contact";
+import DemoRequestArea from "@/components/DemoRequestArea";
+import React from "react";
+
+const DemoRequest = () => {
+	return (
+		<section>
+			<DemoRequestArea />
+			<ContactSection />
+		</section>
+	);
+};
+
+export default DemoRequest;
