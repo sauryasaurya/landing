3883d5cb21081c31d979b7fd169fb8e1db62f23a
Surya Prasad Bhandari <saurya.bhandari78792@gmail.com>
2023-10-09 15:30:07 +0545

    fix: style banner button

--- src/containers/WebAppCreative/Banner/index.js
+++ src/containers/WebAppCreative/Banner/index.js
@@ -4,6 +4,7 @@ import Heading from "@/common/components/Heading";
 import NextImage from "@/common/components/NextImage";
 import Text from "@/common/components/Text";
 import Container from "@/common/components/UI/Container";
+import PropTypes from "prop-types";
 
 import Section, {
 	BannerContent,
@@ -11,7 +12,7 @@ import Section, {
 	Figure,
 } from "./banner.style";
 
-const Banner = () => {
+const Banner = ({ button }) => {
 	return (
 		<Section id="home">
 			<Container width="1400px">
@@ -25,7 +26,7 @@ const Banner = () => {
 							className="animate__animated animate__fadeInUp"
 							content="Velorona is a time tracker and a timesheet app that let's you generate auto Invoice for your clients according to worked hour whithout need of any extra tools."
 						/>
-						<Button title="Get a demo" type="submit" />
+						<Button {...button} title="Get a demo" type="submit" />
 					</BannerContent>
 					<Figure className="animate__animated animate__fadeInUp animate__fast">
 						<NextImage src={dashboard} alt="dashboard" />
@@ -34,6 +35,23 @@ const Banner = () => {
 			</Container>
 		</Section>
 	);
+};
+
+Banner.propTypes = {
+	button: PropTypes.object,
+};
+
+Banner.defaultProps = {
+	button: {
+		type: "button",
+		fontSize: `${2}`,
+		fontWeight: "600",
+		borderRadius: "4px",
+		pl: "22px",
+		pr: "22px",
+		colors: "primaryWithBg",
+		height: `${4}`,
+	},
 };
 
 export default Banner;
